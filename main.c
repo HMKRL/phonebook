8b245cf9fb9f10a4aaa899294ed7cb884e2fbc39
Yu-Cheng Liang <a135246a135246@gmail.com>
2017-10-04 16:27:34 +0800

    Use dj2b hash function for hash table

--- main.c
+++ main.c
@@ -27,6 +27,85 @@ static double diff_in_second(struct timespec t1, struct timespec t2)
     return (diff.tv_sec + diff.tv_nsec / 1000000000.0);
 }
 
+#ifdef OPT /* main for hash version */
+
+int main(int argc, char *argv[])
+{
+    FILE *fp;
+    int i = 0;
+    char line[MAX_LAST_NAME_SIZE];
+    struct timespec start, end;
+    double cpu_time1, cpu_time2;
+
+    /* check file opening */
+    fp = fopen(DICT_FILE, "r");
+    if (fp == NULL) {
+        printf("cannot open the file\n");
+        return -1;
+    }
+
+    /* build the entry */
+    entry *pHead[HASH_TABLE_SIZE];
+    for (i = 0; i < HASH_TABLE_SIZE; ++i) {
+        pHead[i] = (entry *) malloc(sizeof(entry));
+        pHead[i]->pNext = NULL;
+    }
+    printf("size of entry : %lu bytes\n", sizeof(entry));
+
+#if defined(__GNUC__)
+    __builtin___clear_cache((char *) pHead, (char *) pHead + sizeof(entry));
+#endif
+    clock_gettime(CLOCK_REALTIME, &start);
+    while (fgets(line, sizeof(line), fp)) {
+        while (line[i] != '\0')
+            i++;
+        line[i - 1] = '\0';
+        i = 0;
+        append(line, pHead);
+    }
+    clock_gettime(CLOCK_REALTIME, &end);
+    cpu_time1 = diff_in_second(start, end);
+
+    /* close file as soon as possible */
+    fclose(fp);
+
+    /* the givn last name to find */
+    char input[MAX_LAST_NAME_SIZE] = "zyxel";
+
+    assert(findName(input, pHead) &&
+           "Did you implement findName() in " IMPL "?");
+    assert(0 == strcmp(findName(input, pHead)->lastName, "zyxel"));
+
+#if defined(__GNUC__)
+    __builtin___clear_cache((char *) pHead, (char *) pHead + sizeof(entry));
+#endif
+    /* compute the execution time */
+    clock_gettime(CLOCK_REALTIME, &start);
+    findName(input, pHead);
+    clock_gettime(CLOCK_REALTIME, &end);
+    cpu_time2 = diff_in_second(start, end);
+
+    FILE *output = fopen(OUT_FILE, "a");
+    fprintf(output, "append() findName() %lf %lf\n", cpu_time1, cpu_time2);
+    fclose(output);
+
+    printf("execution time of append() : %lf sec\n", cpu_time1);
+    printf("execution time of findName() : %lf sec\n", cpu_time2);
+
+    for (i = 0; i < HASH_TABLE_SIZE; ++i) {
+        entry *temp;
+        while(pHead[i]) {
+            temp = pHead[i]->pNext;
+            free(pHead[i]);
+            pHead[i] = temp;
+        }
+    }
+
+    return 0;
+}
+
+#else /* main for origin version */
+
 int main(int argc, char *argv[])
 {
     FILE *fp;
@@ -97,3 +176,5 @@ int main(int argc, char *argv[])
 
     return 0;
 }
+#endif
+
--- phonebook_opt.c
+++ phonebook_opt.c
@@ -1,15 +1,40 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 #include "phonebook_opt.h"
 
-/* FILL YOUR OWN IMPLEMENTATION HERE! */
-entry *findName(char lastName[], entry *pHead)
+unsigned long hash(unsigned char *str)
 {
-    /* TODO: implement */
+    unsigned long hash = 5381;
+    int c;
+    while (c = *str++) {
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+    }
+
+    return hash;
+}
+
+entry *findName(char lastName[], entry **pArr)
+{
+    entry *pHead = pArr[hash(lastName) % HASH_TABLE_SIZE];
+    while (pHead != NULL) {
+        if (strcasecmp(lastName, pHead->lastName) == 0)
+            return pHead;
+        pHead = pHead->pNext;
+    }
     return NULL;
 }
 
-entry *append(char lastName[], entry *e)
+entry *append(char lastName[], entry **pArr)
 {
-    return NULL;
+    /* allocate memory for the new entry and put lastName */
+    int index = hash(lastName) % HASH_TABLE_SIZE;
+    entry *e = (entry *) malloc(sizeof(entry));
+    e->pNext = pArr[index];
+    pArr[index] = e;
+    strcpy(e->lastName, lastName);
+
+    return e;
 }
